38fab36bb762f57fceb5ee81e9096511eec97434
Andrei Ignat <ignatandrei@yahoo.com>
2026-09-10 23:45:37 +0300

    docs

--- src/EFCore_DaIn/10.0.11/EFCore_DaIn_10/IEFCore_DatabasePlugin_10.cs
+++ src/EFCore_DaIn/10.0.11/EFCore_DaIn_10/IEFCore_DatabasePlugin_10.cs
@@ -1,14 +1,36 @@
 ﻿using EFCore_DaIn;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection.PortableExecutable;
 
 namespace EFCore_DaIn_10;
 
 public interface IEFCore_DatabasePlugin : IDatabasePlugin
 {
-    
+    /// <summary>
+    /// to be used 
+    /// var optBuilder = plugin.GenerateDbContextOptionsBuilder<EmpContext>(cn, "EmpContextDatabase");
+    /// var opt = optBuilder.Options;
+    /// EmpContext context = new EmpContext(opt);
+    ///
+    /// </summary>
+    /// <typeparam name="T"> the context from EF</typeparam>
+    /// <param name="connectionString">connection string</param>
+    /// <param name="databaseName">database name, optional</param>
+    /// <returns></returns>
     DbContextOptionsBuilder<T> GenerateDbContextOptionsBuilder<T>(string connectionString, string? databaseName = null)
         where T : DbContext;
 
+    /// <summary>
+    /// to be used
+    /// services.AddDbContextFactory<ApplicationDbContext>(
+    ///options => plugin.UsePlugin(options, connectionString));
+    /// </summary>
+    /// <typeparam name="T"> the context from EF</typeparam>
+    /// <param name="optionBuilder">the DbContextOptionsBuilder</param>
+    /// <param name="connectionString">connection string</param>
+    /// <param name="databaseName">database name, optional</param>
+    /// <returns></returns>
     DbContextOptionsBuilder<T>  UsePlugin<T>(DbContextOptionsBuilder<T> optionBuilder,string connectionString, string? databaseName = null)
         where T : DbContext;    
 
